76c76c28900d5dfb982f35457edf68f523df84f9
desvillena <desarrollovillena@gmail.com>
2021-10-16 04:21:06 +0200

    Sergios Style

--- templates/header.php
+++ templates/header.php
@@ -2,7 +2,7 @@
     <div class="container_header">
         <div class="menuLogo"><a href="index.php"><img src="IMAGES/tittleImage.png"
                     onerror="this.src='../IMAGES/tittleImage.png';" width="50px" height="50px"></a></div>
-        <div class="menusearch">
+        <div class="menuSearch">
             <form>
                 <input type="text" size="30" onkeyup="showResult(this.value)">
                 <div id="livesearch"></div>
