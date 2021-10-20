721cb142b9aab7e51d7de1694a0695160baa762e
ADCairex <angelmartinezguardiola@gmail.com>
2021-10-20 17:07:55 +0200

    testUploadContent

--- index.php
+++ index.php
@@ -6,16 +6,21 @@
         <?php
             include 'css/styles.php';
         ?>
+        <script src="src/js/jquery-3.6.0.js"></script>
+        <script src="src/js/testUploadContent.js"></script>
     </head>
     <body>
         <?php
+            /*
             include 'templates/header.php';
             include 'templates/algorithm.php';
             include 'templates/footer.php';
             include 'scripts.php';
+            */
         ?>
-        <div class="content">
+        <div class="content" id="content">
             
         </div>
+        <button id="click">Click</button>
     </body>
 </html>
---
+++ src/js/testUploadContent.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#click").click(function() {
+        $("#content").load('templates/algorithm.php');
+    });
+});
