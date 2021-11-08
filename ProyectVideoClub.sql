dff45a672133387142576c0fe3d79d4d278e5029
ADCairex <angelmartinezguardiola@gmail.com>
2021-11-08 19:19:19 +0100

    x

--- ProyectVideoClub.sql
+++ ProyectVideoClub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 19-10-2021 a las 01:14:09
+-- Tiempo de generación: 08-11-2021 a las 19:18:43
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 7.4.24
 
@@ -33,17 +33,24 @@ CREATE TABLE `Bill` (
   `total` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `BuyUserProduct`
+-- Volcado de datos para la tabla `Bill`
 --
 
-CREATE TABLE `BuyUserProduct` (
-  `idBuyUserProduct` int(11) NOT NULL,
-  `idUser` int(11) NOT NULL,
-  `idProduct` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `Bill` (`idBill`, `idUser`, `total`) VALUES
+(1, 3, '2'),
+(2, 3, '4'),
+(3, 3, '1'),
+(4, 3, '2'),
+(5, 3, '1'),
+(6, 3, '1'),
+(7, 3, '1'),
+(8, 3, '1'),
+(9, 3, '1'),
+(10, 3, '1'),
+(11, 3, '1'),
+(12, 3, '11'),
+(13, 3, '11');
 
 -- --------------------------------------------------------
 
@@ -68,34 +75,27 @@ CREATE TABLE `LineSale` (
   `idBill` int(11) NOT NULL,
   `idProduct` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `price` decimal(10,0) NOT NULL
+  `linePrice` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `Product`
+-- Volcado de datos para la tabla `LineSale`
 --
 
-CREATE TABLE `Product` (
-  `idProduct` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `idAuthor` int(11) NOT NULL,
-  `price` decimal(10,0) NOT NULL,
-  `routProduct` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ProductCategories`
---
-
-CREATE TABLE `ProductCategories` (
-  `idProductCategories` int(11) NOT NULL,
-  `idCategory` int(11) NOT NULL,
-  `idProduct` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `LineSale` (`idLineSale`, `idBill`, `idProduct`, `quantity`, `linePrice`) VALUES
+(1, 1, 11, 1, '2'),
+(1, 2, 11, 1, '2'),
+(1, 3, 11, 1, '1'),
+(1, 4, 11, 1, '1'),
+(1, 5, 11, 1, '1'),
+(1, 6, 11, 1, '1'),
+(1, 7, 11, 1, '1'),
+(1, 8, 11, 1, '1'),
+(1, 11, 11, 1, '1'),
+(1, 12, 11, 1, '11'),
+(1, 13, 11, 1, '11'),
+(2, 2, 12, 1, '2'),
+(2, 4, 12, 1, '1');
 
 -- --------------------------------------------------------
 
@@ -109,28 +109,16 @@ CREATE TABLE `User` (
   `pass` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `surnames` varchar(255) NOT NULL,
-  `addr` varchar(255) NOT NULL
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `User`
 --
 
-INSERT INTO `User` (`idUser`, `username`, `pass`, `name`, `surnames`, `addr`) VALUES
-(2, 'cairex', 'd', 'd', 'd', 'd');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ViewUserProduct`
---
-
-CREATE TABLE `ViewUserProduct` (
-  `idViewUserProduct` int(11) NOT NULL,
-  `idUser` int(11) NOT NULL,
-  `idProduct` int(11) NOT NULL,
-  `numViews` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `User` (`idUser`, `username`, `pass`, `name`, `surnames`, `email`) VALUES
+(2, 'cairex', 'd', 'd', 'd', 'd'),
+(3, 'ADCairex', '123', 'Angel', 'Martinez', 'Calle');
 
 --
 -- Índices para tablas volcadas
@@ -142,14 +130,6 @@ CREATE TABLE `ViewUserProduct` (
 ALTER TABLE `Bill`
   ADD PRIMARY KEY (`idBill`),
   ADD KEY `Bill_fk0` (`idUser`);
-
---
--- Indices de la tabla `BuyUserProduct`
---
-ALTER TABLE `BuyUserProduct`
-  ADD PRIMARY KEY (`idBuyUserProduct`),
-  ADD KEY `BuyUserProduct_fk0` (`idUser`),
-  ADD KEY `BuyUserProduct_fk1` (`idProduct`);
 
 --
 -- Indices de la tabla `Category`
@@ -166,33 +146,11 @@ ALTER TABLE `LineSale`
   ADD KEY `LineSale_fk1` (`idProduct`);
 
 --
--- Indices de la tabla `Product`
---
-ALTER TABLE `Product`
-  ADD PRIMARY KEY (`idProduct`);
-
---
--- Indices de la tabla `ProductCategories`
---
-ALTER TABLE `ProductCategories`
-  ADD PRIMARY KEY (`idProductCategories`) USING BTREE,
-  ADD KEY `ProductCategories_fk0` (`idCategory`),
-  ADD KEY `ProductCategories_fk1` (`idProduct`);
-
---
 -- Indices de la tabla `User`
 --
 ALTER TABLE `User`
   ADD PRIMARY KEY (`idUser`),
   ADD UNIQUE KEY `username` (`username`);
-
---
--- Indices de la tabla `ViewUserProduct`
---
-ALTER TABLE `ViewUserProduct`
-  ADD PRIMARY KEY (`idViewUserProduct`),
-  ADD KEY `ViewUserProduct_fk0` (`idUser`),
-  ADD KEY `ViewUserProduct_fk1` (`idProduct`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -202,13 +160,7 @@ ALTER TABLE `ViewUserProduct`
 -- AUTO_INCREMENT de la tabla `Bill`
 --
 ALTER TABLE `Bill`
-  MODIFY `idBill` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `BuyUserProduct`
---
-ALTER TABLE `BuyUserProduct`
-  MODIFY `idBuyUserProduct` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idBill` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `Category`
@@ -217,22 +169,10 @@ ALTER TABLE `Category`
   MODIFY `idCategory` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `Product`
---
-ALTER TABLE `Product`
-  MODIFY `idProduct` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
 -- AUTO_INCREMENT de la tabla `User`
 --
 ALTER TABLE `User`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `ViewUserProduct`
---
-ALTER TABLE `ViewUserProduct`
-  MODIFY `idViewUserProduct` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
@@ -245,32 +185,11 @@ ALTER TABLE `Bill`
   ADD CONSTRAINT `Bill_fk0` FOREIGN KEY (`idUser`) REFERENCES `User` (`idUser`);
 
 --
--- Filtros para la tabla `BuyUserProduct`
---
-ALTER TABLE `BuyUserProduct`
-  ADD CONSTRAINT `BuyUserProduct_fk0` FOREIGN KEY (`idUser`) REFERENCES `User` (`idUser`),
-  ADD CONSTRAINT `BuyUserProduct_fk1` FOREIGN KEY (`idProduct`) REFERENCES `Product` (`idProduct`);
-
---
 -- Filtros para la tabla `LineSale`
 --
 ALTER TABLE `LineSale`
   ADD CONSTRAINT `LineSale_fk0` FOREIGN KEY (`idBill`) REFERENCES `Bill` (`idBill`),
   ADD CONSTRAINT `LineSale_fk1` FOREIGN KEY (`idProduct`) REFERENCES `Product` (`idProduct`);
-
---
--- Filtros para la tabla `ProductCategories`
---
-ALTER TABLE `ProductCategories`
-  ADD CONSTRAINT `ProductCategories_fk0` FOREIGN KEY (`idCategory`) REFERENCES `Category` (`idCategory`),
-  ADD CONSTRAINT `ProductCategories_fk1` FOREIGN KEY (`idProduct`) REFERENCES `Product` (`idProduct`);
-
---
--- Filtros para la tabla `ViewUserProduct`
---
-ALTER TABLE `ViewUserProduct`
-  ADD CONSTRAINT `ViewUserProduct_fk0` FOREIGN KEY (`idUser`) REFERENCES `User` (`idUser`),
-  ADD CONSTRAINT `ViewUserProduct_fk1` FOREIGN KEY (`idProduct`) REFERENCES `Product` (`idProduct`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
